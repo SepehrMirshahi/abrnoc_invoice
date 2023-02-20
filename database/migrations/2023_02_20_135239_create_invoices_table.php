5f56cdc0556a17c4345e2cc6589ff108f199384e
Sepehr Mirshahi <sepehr.mirshahi@outlook.com>
2023-02-20 17:42:39 +0330

    Database tables created

--- database/migrations/2023_02_20_135239_create_invoices_table.php
+++ database/migrations/2023_02_20_135239_create_invoices_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Subscription;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,11 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('password');
-            $table->bigInteger('credit');
+            $table->foreignIdFor(Subscription::class)->constrained();
+            $table->timestamps();
         });
     }
 
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('invoices');
     }
 };
