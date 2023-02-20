863940415bbfdc6d8f09b1a975aeef3c6921a26a
Sepehr Mirshahi <sepehr.mirshahi@outlook.com>
2023-02-20 18:14:58 +0330

    Model relations created

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Subscription extends Model
 {
     use HasFactory;
+
+    public function user(){
+        $this->belongsTo(User::class);
+    }
+
+    public function invoice(){
+        $this->hasMany(Invoice::class);
+    }
 }
